8fd8cc3ea96beea616f13ca820b6c9a932238970
Kim Schaefer <kschaefer@me.com>
2025-06-21 13:09:43 -0600

    add header and placeholder components

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,34 +1,40 @@
-import {initialTickets} from "@/data";
+import Link from "next/link";
+
+import { Placeholder } from "@/components/placeholder";
+import { Button } from "@/components/ui/button";
+import { initialTickets } from "@/data";
+import { ticketsPath } from "@/paths";
 
 type TicketPageProps = {
-    params: Promise<{
-        ticketId: string;
-    }>;
+  params: Promise<{
+    ticketId: string;
+  }>;
 };
 
-export default async function TicketPage({ params }: TicketPageProps) {
-    const {ticketId} = await params;
-    const ticket = initialTickets.find(ticket => ticket.id === ticketId);
+const TicketPage = async ({ params }: TicketPageProps) => {
+  const { ticketId } = await params;
 
-    if (!ticket) {
-    return (
-          <div className="flex flex-col gap-[16px] items-center sm:items-start">
-            <h2 className="text-4xl font-bold">Ticket Not Found</h2>
-          </div>
-    );
-    }
+  const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
 
+  if (!ticket) {
     return (
-      <div className="flex-1 flex flex-col gap-y-8">
-      <div>
-        <h2 className="text-3xl font-bold tracking-tight">{ticket.title}</h2>
-        <p className="text-sm text-muted-foreground">
-          {ticket.content}
-        </p>
-        <p className="text-sm text-muted-foreground">
-          Ticket Id: {ticket.id}
-        </p>
-      </div>
-    </div>
+      <Placeholder
+        label="Ticket not found"
+        button={
+          <Button asChild variant="outline">
+            <Link href={ticketsPath()}>Go to Tickets</Link>
+          </Button>
+        }
+      />
     );
   }
+
+  return (
+    <div>
+      <h2 className="text-lg">{ticket.title}</h2>
+      <p className="text-sm">{ticket.content}</p>
+    </div>
+  );
+};
+
+export default TicketPage;
