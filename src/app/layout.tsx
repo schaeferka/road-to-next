9c2508f009b1423ea3ffd94102619e6d869fa8ba
Kim Schaefer <kschaefer@me.com>
2025-06-21 11:49:41 -0600

    tickets page card and layout updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,18 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Link from "next/link";
 
+import { buttonVariants } from "@/components/ui/button";
 import { homePath, ticketsPath } from "@/paths";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  weight: "100 900"
+  weight: "100 900",
 });
-
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
-  weight: "100 900"
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
@@ -33,24 +33,41 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav className="supports-backdrop-blur:bg-background/60
-                fixed left-0 right-0 top-0 z-20
-                border-b bg-backdrop/95 backdrop-blur
-                w-full flex py-2.5 px-5 justify-between">
+        <nav
+          className="
+            supports-backdrop-blur:bg-background/60
+            fixed left-0 right-0 top-0 z-20
+            border-b bg-background/95 backdrop-blur
+            w-full flex py-2.5 px-5 justify-between
+          "
+        >
           <div>
-            <Link href={homePath()} className="text-lg font-bold">
+            <Link
+              href={homePath()}
+              className={buttonVariants({ variant: "outline" })}
+            >
               Home
             </Link>
           </div>
           <div>
-            <Link href={ticketsPath()} className="text-lg font-bold">
+            <Link
+              href={ticketsPath()}
+              className={buttonVariants({ variant: "outline" })}
+            >
               Tickets
             </Link>
           </div>
         </nav>
-        <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden
-                  py-24 px-8 bg-secondary/20 flex flex-col">
-        {children}
+        <main
+          className="
+            min-h-screen flex-1
+            overflow-y-auto overflow-x-hidden
+            py-24 px-8
+            bg-secondary/20
+            flex flex-col
+          "
+        >
+          {children}
         </main>
       </body>
     </html>
