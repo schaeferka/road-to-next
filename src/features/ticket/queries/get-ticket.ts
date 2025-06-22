356901b263df79d547220c73e3dc95a8f1d716de
Kim Schaefer <kschaefer@me.com>
2025-06-21 20:14:11 -0600

    tickets list suspense updates

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -3,7 +3,7 @@ import { initialTickets } from "@/data";
 import { Ticket } from "../types";
 
 export const getTicket = async (id: string): Promise<Ticket | null> => {
-  await new Promise((resolve) => setTimeout(resolve, 200));
+  await new Promise((resolve) => setTimeout(resolve, 2000));
 
   const maybeTicket = initialTickets.find((ticket) => ticket.id === id);
 
