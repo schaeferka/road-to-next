8e5d8d843389efb335ae705d05690dd5c58725c8
Kim Schaefer <kschaefer@me.com>
2025-06-21 20:39:31 -0600

    add ticket not found page

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
+import { notFound } from "next/navigation";
 
-import { Placeholder } from "@/components/placeholder";
-import { Button } from "@/components/ui/button";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
-import { ticketsPath } from "@/paths";
 
 type TicketPageProps = {
   params: Promise<{
@@ -17,23 +14,13 @@ const TicketPage = async ({ params }: TicketPageProps) => {
   const ticket = await getTicket(ticketId);
 
   if (!ticket) {
-    return (
-      <Placeholder
-        label="Ticket not found"
-        button={
-          <Button asChild variant="outline">
-            <Link href={ticketsPath()}>Go to Tickets</Link>
-          </Button>
-        }
-      />
-    );
+    notFound();
   }
 
   return (
     <div className="flex justify-center animate-fade-from-top">
       <TicketItem ticket={ticket} isDetail />
     </div>
-    
   );
 };
 
