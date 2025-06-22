31f0c9d7b22b4e167bd53abf7d2f0afc0678b76d
Kim Schaefer <kschaefer@me.com>
2025-06-22 14:28:59 -0600

    change to upsert form and action

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,10 +3,10 @@ import { Suspense } from "react";
 import { CardCompact } from "@/components/card-compact";
 import { Heading } from "@/components/heading";
 import { Spinner } from "@/components/spinner";
-import { TicketCreateForm } from "@/features/ticket/components/ticket-create-form";
 import { TicketList } from "@/features/ticket/components/ticket-list";
+import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
 
-const TicketsPage = async () => {
+async function TicketsPage() {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
@@ -15,14 +15,13 @@ const TicketsPage = async () => {
         title="Create Ticket"
         description="A new ticket will be created"
         className="w-full max-w-[420px] self-center"
-        content={<TicketCreateForm />}
-      />
+        content={<TicketUpsertForm />} />
 
       <Suspense fallback={<Spinner />}>
         <TicketList />
       </Suspense>
     </div>
   );
-};
+}
 
 export default TicketsPage;
